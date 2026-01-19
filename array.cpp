29a5456061a7b85182261b77ea5f578b1f607949
osama4959 <osamaomran890@gmail.com>
2026-01-19 23:38:56 +0200

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+intmain() {
+
+int arr[5] = {1, 2, 3, 4, 5};
+cout << "Array elements are: ";
+for(int i =0 ; i<5; i++){
+cout << arr[i] << " ";
+return 0;
+
+}
+
+}
